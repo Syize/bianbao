29bf0977f10b85c02e344a81d81ce2151a0081b1
Syize <15610389800@163.com>
2020-04-30 23:15:03 +0800

    add new function,no test

--- get_result.py
+++ get_result.py
@@ -2,6 +2,22 @@
 #-*- coding:utf-8 -*-
 
 from sys import exit
+#from multiprocessing import Queue,Process
+############计算相似度################
+#def cal1(tag,queue):
+#    sums=0
+#    for i in tag:
+#        sums+=ord(i)
+#    queue.put(sums)
+#    
+#def calculate(tag1,tag2):
+#    que1=Queue();que2=Queue()
+#    p1=Process(target=cal1,args=(tag1,que1))
+#    p2=Process(target=cal1,args=(tag2,que2))
+#    p1.start();p2.start();p1.join();p2.join()
+#    if abs(que1.get()-que2.get())<19:
+#        print(tag1,end='  ')
+
 #########1段###########################
 
 def is6r(): #是否编报降水组6RRR1
@@ -82,7 +98,14 @@ def yun_volume():   #云量
 def wind_loca():    #风向
     loca=input('\n风向(字母)\n静风可输入0:')
     dic={'0':'00','NNE':'02','NE':'04','ENE':'07','E':'09','ESE':'11','SE':'14','SSE':'16','S':'18','SSW':'20','SW':'22','WSW':'25','W':'27','WNW':'29','NW':'32','NNW':'34','N':'36'}
-    return dic[loca.upper()]
+    try:
+        return dic[loca.upper()]
+    except KeyError:
+        print('输入有误，请参考以下键值:')
+        for i in dic.keys():
+            print(i,end='  ')
+        print('')
+        return -1
 
 def wind_velo():    #风速
     velo=input('\n风速(整数)\n小数四舍五入,个位前面加0，如:09,18:')
@@ -101,6 +124,29 @@ def pres():     #气压
     pres=input('\n气压\n以0.1hpa为单位，应为4位整数\n千位请舍去，如1024.5只填“0245”:')
     return flag+pres
 
+def pres_chan():    #气压变化倾向，变化量
+    while 1:
+        which=input('\n过去三小时气压变化倾向:\n2.上升 4.不变 7.下降:')
+        if which in ['2','4','7']:
+            break
+        else:
+            print('输入有误!')
+    many=input('\n气压变化量，以0.1hpa为单位\n千位舍去，应为三位整数:')
+    print('结果:','5'+which+many)
+
+def rain(flag):     #降水量
+    if flag=='1':
+        volume=input('\n降水量，以毫米为单位:')
+        if volume<1:
+            print('结果为:','6'+str(990+int(int(volume)*10))+'1')
+        else:
+            for i in range(len(volume)):
+                volume='0'+volume
+            print('结果为:','6'+volume+'1')
+    else:
+        pass
+
+def 
 ###########3段##############################
 
 def is3():
@@ -127,6 +173,7 @@ def iihvv():    #
             tag2=str(tag)
             break
     print('结果:',tag3+tag4+tag1+tag2)
+    return tag3,tag4
 
 def nddff():    #
     while 1:
@@ -147,7 +194,7 @@ def nddff():    #
     print('结果:',tag1+tag2+tag3)
 
 if __name__=='__main__':
-    iihvv();nddff();
+    6r,7w=iihvv();nddff();
     while 1:
         tag=temp()
         if tag!=-1:
@@ -158,4 +205,5 @@ if __name__=='__main__':
         if tag!=-1:
             print(str(tag))
             break
+    pres_chan();rain(6r);
     print('\n编报结果已全部输出！')
